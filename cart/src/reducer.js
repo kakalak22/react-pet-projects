677ce3b56eab8361d3d608953af66ac8608b6b03
kakalak11 <tr.gia.bao.94@gmail.com>
2022-06-16 17:06:07 +0700

    fix Cart: amount increase twice

--- cart/src/reducer.js
+++ cart/src/reducer.js
@@ -1,42 +1,64 @@
 const reducer = (state, action) => {
+  if (action.type === "LOADING") {
+    return { ...state, loading: true };
+  }
 
-    console.log(state, action);
+  if (action.type === "DISPLAY_ITEM") {
+    return { ...state, cart: action.payload, loading: false };
+  }
 
-    if (action.type === 'LOADING') {
-        return { ...state, loading: true };
-    }
+  if (action.type === "GET_TOTAL") {
+    const total = state.cart.reduce(
+      (prevVal, currentVal) => prevVal + currentVal.amount * currentVal.price,
+      0
+    );
+    const roundedTotal = Number(total).toFixed(2);
+    const count = state.cart.reduce(
+      (prevVal, currentVal) => prevVal + currentVal.amount,
+      0
+    );
+    return { ...state, total: roundedTotal, count: count };
+  }
 
-    if (action.type === 'DISPLAY_ITEM') {
-        return { ...state, cart: action.payload, loading: false };
-    }
+  if (action.type === "REMOVE_ITEM") {
+    const newCart = state.cart.filter((item) => item.id !== action.payload);
+    return { ...state, cart: newCart };
+  }
 
-    if (action.type === 'GET_TOTAL') {
-        const total = state.cart.reduce((prevVal, currentVal) => (prevVal + currentVal.amount * currentVal.price), 0);
-        const roundedTotal = Number(total).toFixed(2);
-        const count = state.cart.reduce((prevVal, currentVal) => (prevVal + currentVal.amount), 0);
-        return { ...state, total: roundedTotal, count: count };
-    }
+  if (action.type === "INCREASE") {
+    const newCart = state.cart.map((item) =>
+      item.id === action.payload
+        ? { ...item, amount: item.amount + 1 }
+        : { ...item }
+    );
+    return { ...state, cart: newCart };
+  }
 
-    if (action.type === 'REMOVE_ITEM') {
-        const newCart = state.cart.filter(item => item.id !== action.payload);
-        return { ...state, cart: newCart };
-    }
+  if (action.type === "DECREASE") {
+    const newCart = state.cart
+      .map((item) =>
+        item.id === action.payload
+          ? { ...item, amount: item.amount - 1 }
+          : { ...item }
+      )
+      .filter(({ amount }) => amount !== 0);
+    return { ...state, cart: newCart };
+  }
 
-    if (action.type === 'INCREASE') {
-        const index = state.cart.findIndex(item => item.id === action.payload);
-        const newCart = [...state.cart];
-        newCart[index].amount = state.cart[index].amount + 1;
-        return { ...state, cart: newCart };
-    }
+  if (action.type === "TOGGLE_AMOUNT") {
+    const newCart = state.cart
+      .map((item) =>
+        item.id === action.payload.id
+          ? action.payload.type === "inc"
+            ? { ...item, amount: item.amount + 1 }
+            : { ...item, amount: item.amount - 1 }
+          : { ...item }
+      )
+      .filter(({ amount }) => amount !== 0);
+    return { ...state, cart: newCart };
+  }
 
-    if (action.type === 'DECREASE') {
-        const index = state.cart.findIndex(item => item.id === action.payload);
-        const tempCart = [...state.cart];
-        tempCart[index].amount = state.cart[index].amount - 1;
-        const newCart = tempCart.filter(({ amount }) => amount !== 0)
-        return { state, cart: newCart };
-    }
-    throw new Error('no matching action type');
-}
+  throw new Error("no matching action type");
+};
 
-export default reducer
+export default reducer;
